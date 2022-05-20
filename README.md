8a7ff98c53ec3eab32ca30837823ea8c3eec2259
Joanna <jlapa.transport@gmail.com>
2022-05-20 16:41:12 +0200

    API function made in js, layout details corrected

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 https://css-tricks.com/best-way-implement-wrapper-css/
 https://css-tricks.com/adding-stroke-to-web-text/
 https://developer.mozilla.org/en-US/docs/Web/CSS/filter-function/drop-shadow
+https://developer.mozilla.org/en-US/docs/Web/API/Request/cache - how to resolve the problem with default using the cache. Setting 'no-cache' makes that ' the browser makes a conditional request to the remote server. If the server indicates that the resource has not changed, it will be returned from the cache. Otherwise the resource will be downloaded from the server and the cache will be updated.'
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,27 @@
-'use strict';
+'use strict'
 
+let adviceNumber = document.querySelector('#advice-number')
+let adviceText = document.querySelector('#advice')
+const button = document.querySelector('#button')
+
+let URL = 'https://api.adviceslip.com/advice'
+
+function getAdvice() {
+	fetch(URL, {cache: "no-cache"})
+		.then(res => {
+			return res.json()
+		})
+		.then(adviceData => {
+			const adviceObject = adviceData.slip
+			adviceNumber.textContent = 'ADVICE #' + adviceObject.id
+			adviceText.textContent = '"' + adviceObject.advice + '"'
+		})
+		.catch(error => console.log('error'))
+}
+
+button.addEventListener('click', () => {
+	getAdvice()
+})
+window.onload = () => {
+	getAdvice()
+}
